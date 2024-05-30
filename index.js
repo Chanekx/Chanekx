6d32f5e581915cca0389a2f592948d95a40801fb
chanekx <mhechrist@gmail.com>
2024-05-30 21:30:54 +0800

    added sticky navbar

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function reloadFile(){
     location.reload();
     console.log("Page Reloaded");
 }
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Downscroll
+        navbar.classList.add('hidden');
+    } else {
+        // Upscroll
+        navbar.classList.remove('hidden');
+    }
+    lastScrollTop = scrollTop;
+});
